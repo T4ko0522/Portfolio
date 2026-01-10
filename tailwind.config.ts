1afa3bd62bd48b12dcc21230f25ee47d26cffad4
Tako <tako.renraku1@gmail.com>
2026-01-10 23:00:11 +0900

    feat: Tailwindの設定にアニメーションのキーフレームを追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,10 +87,37 @@ const config = {
   				}
   			}
   		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+  			'text-reveal': {
+  				'0%': {
+  					clipPath: 'inset(0 100% 0 0)'
+  				},
+  				'100%': {
+  					clipPath: 'inset(0 0% 0 0)'
+  				}
+  			}
+  		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'blink': 'blink 1s step-end infinite'
+  			'blink': 'blink 1s step-end infinite',
+  			'text-reveal': 'text-reveal 0.5s ease-out forwards'
   		}
   	}
   },
